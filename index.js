345979e2a61242813e6ed8d7101176765bb8c82c
joa242312 <joaogabrielss2006@gmail.com>
2025-05-30 12:01:37 -0300

    app novas

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express")
 const app = express()
 const port = 6579
+require("dotenv").config()
 const { Pool } = require("pg")
 
 const pool = new Pool({
-    user: 'postgres.nvwruviltrndiaimeejc',
-    host: 'aws-0-us-east-2.pooler.supabase.com',
-    database: 'postgres',
-    password: "Jg162006@",
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NOME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
 
 })
 
@@ -26,28 +27,51 @@ app.post('/probutos', async (req, res) => {
     }
     try {
         const probuto = await pool.query(`
-INSERT INTO probutos(nome,preco,categoria,image_url)
-VALUES(
-'${nome}',
-'${preco}',
-'${categoria}',
-'${image_url}' 
-)
-RETURNING *
-`)
+     INSERT INTO probutos(nome,preco,categoria,image_url)
+      VALUES($1 $2 $3 $4)
+      RETURNING *
+      `, (nome, preco, categoria, image_url))
         res.status(201).send(probuto.rows[0])
     } catch (error) {
         console.error(error)
         return res.status(500).send("error  ao cadastrar o produtos")
     }
 })
-app.get("/probutos", async (req,res) => {
+app.get("/probutos", async (req, res) => {
     try {
         const probutos = await pool.query("SELECT * FROM probutos")
         return res.status(200).send(probutos.rows)
-        } catch (error) { 
+    } catch (error) {
         console.error(error)
         return res.status(500).send("Error ao buscar probutos")
+    }
+})
+app.get('/probutos/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const probuto = await pool.query(`
+            SELECT * FROM probutos WHERE id = $1`,(id))
+        if (!probuto.rows.length) { return res.status(400).sand('Produtos não encontados') }
+        return res.send(probuto.rows[0])
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('error ao buscar probuto')
+    }
+})
+app.delete('/probutos/:id', async (req, res) => {
+    const probutos = await pool.query(`SELECT * FROM probustos WHERE id= $1`,(id))
+    if (probutos.rows.length) {
+        return res.status(404).send('produtos não encontrado')
+    }
+    const { id } = req.params
+    try {
+        await pool.query(`
+        DELETE FROM probutos WHERE id = $1`, (id))
+        return res.status(202).send('Produtos deletado com sucesso')
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('error ao deletar o produto')
     }
 })
 
